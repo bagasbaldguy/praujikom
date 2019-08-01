f19a2a75f5c16be1bcab4623c1646dd016b7ee7e
bagasbaldguy <bagasbaldguy5@gmail.com>
2019-08-01 22:03:11 +0700

    Set Welcome Message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bagas I M
+                    Perpustakaan (Dalam Proses)...
                 </div>
 
                 <div class="links">
